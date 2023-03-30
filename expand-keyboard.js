d3dd4b522b36a55e1cc73de0d890d86c9348f27b
nbe <aleisa.naser@gmail.com>
2023-03-30 19:02:22 +0300

    Initial commit

---
+++ expand-keyboard.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         Expand keyboard
+// @namespace    http://tampermonkey.net/
+// @match        https://configure.zsa.io/train/home
+// @version      0.1
+// @description  Automatically expand the keyboard on the ZSA Oryx train page
+// @author       thenbe (https://github.com/thenbe)
+// @grant        none
+// ==/UserScript==
+
+(function () {
+	'use strict';
+
+	// Function to perform the click and log the result
+	function autoClickIconShrink() {
+		var iconShrinkElement = document.querySelector('.icon-shrink');
+
+		if (iconShrinkElement) {
+			iconShrinkElement.click();
+			console.log('Click successful: The .icon-shrink element was clicked.');
+		} else {
+			console.log('Click failed: The .icon-shrink element was not found.');
+		}
+	}
+
+	// Function to observe DOM changes and trigger the click when the target element is detected
+	function observeDOM() {
+		const targetNode = document.body;
+
+		const config = {
+			childList: true,
+			subtree: true,
+		};
+
+		const observer = new MutationObserver((mutationsList, observer) => {
+			if (document.querySelector('.icon-shrink')) {
+				autoClickIconShrink();
+				observer.disconnect();
+			}
+		});
+
+		observer.observe(targetNode, config);
+	}
+
+	// Check if the page matches and observe DOM changes
+	if (window.location.href === 'https://configure.zsa.io/train/home') {
+		console.debug('Page matches');
+		observeDOM();
+	}
+})();
